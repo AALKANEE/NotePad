2c139291a1b9de6404f68d21426b3063af5098d7
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-04-01 13:02:24 +0330

    add remove button

--- app.js
+++ app.js
@@ -19,7 +19,22 @@ function newNote(e){
     e.preventDefault()
     //access to the value
     const note = document.querySelector('#note').value
+
+    //create remove element
+    const removeBtn = document.createElement('a')
+    removeBtn.textContent='X'
+    console.log(removeBtn)
     
     //create <li> tag
     const li=document.createElement('li')
+    li.appendChild(document.createTextNode(note))
+
+    //adding remove button to the li
+    li.appendChild(removeBtn)
+
+    // adding li to the note list
+    noteList.appendChild(li)
+
+    console.log(li)
+    console.log(noteList)
 }
