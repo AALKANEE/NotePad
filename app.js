a7baa5db525c50b219eb7c6f8ad89ec684f8ceef
AALKANEE <142936918+AALKANEE@users.noreply.github.com>
2024-04-01 12:50:08 +0330

    add li tag and note List

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // variables
+const noteList = document.querySelector('#note-list')
 
 
 
 
 //eventlisteners
 eventlisteners()
+//form submission
 function eventlisteners(){
     document.querySelector('#form').addEventListener('submit',newNote)
 }
@@ -12,6 +14,12 @@ function eventlisteners(){
 
 
 //functions
+//Adding new note 
 function newNote(e){
     e.preventDefault()
+    //access to the value
+    const note = document.querySelector('#note').value
+    
+    //create <li> tag
+    const li=document.createElement('li')
 }
